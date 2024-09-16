021d6e0d3eafb2c00fa73f513765265baa6b19f4
zhaburaaya <zhaburaaya@sibur.ru>
2024-09-16 14:10:03 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,6 +2,11 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        for (int i = 2; i < 10; i++) {
+            int x = 1 * i;
+            System.out.println("1 * " + i + " = " + x);
+        }
+
+
     }
 }
