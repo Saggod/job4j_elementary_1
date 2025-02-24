05fd81d9d95c6f89754cf52ca3db5de8b2d3b86b
zhaburaaya <zhaburaaya@sibur.ru>
2025-02-24 14:33:14 +0300

    3.1. Переназначение переменной [#232354 #206412]

--- src/test/java/ru/job4j/MainTest.java
+++ src/test/java/ru/job4j/MainTest.java
@@ -4,10 +4,19 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class MainTest {
+
     @Test
     public void whenActionThenResult() {
         int excepted = 1;
         int result = 1;
         assertThat(result).isEqualTo(excepted);
+    }
+
+    @Test
+    public void changeSizeValue() {
+        int size = 10;
+        size = 100;
+        size = size - 5;
+        System.out.println("size = " + size);
     }
 }
