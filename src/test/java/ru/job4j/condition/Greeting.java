85c551cb6f29520df9259fb454bc8708700fd410
zhaburaaya <zhaburaaya@sibur.ru>
2025-03-04 16:31:25 +0300

    9.1. String [#237407 #206831]

---
+++ src/test/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+import java.time.Year;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        int year = 2025;
+        idea += " But I am a newbie. ";
+        idea += year;
+        System.out.println(idea);
+
+    }
+}
